331b2df96f26c29414fc4a756baac8c86a2f4693
mvllow <mvllow@icloud.com>
2022-09-15 20:04:16 -0400

    update highlights

--- rose-pine.fish
+++ rose-pine.fish
@@ -1,45 +1,41 @@
 # syntax highlighting variables
 # https://fishshell.com/docs/current/interactive.html#syntax-highlighting-variables
-set -gx fish_color_normal normal
+set -gx fish_color_normal e0def4
 set -gx fish_color_command c4a7e7
-# set -gx fish_color_keyword c4a7e7
+set -gx fish_color_keyword 9ccfd8
 set -gx fish_color_quote f6c177
 set -gx fish_color_redirection 31748f
-set -gx fish_color_end c4a7e7
+set -gx fish_color_end 908caa
 set -gx fish_color_error eb6f92
-set -gx fish_color_param e0def4
+set -gx fish_color_param ebbcba
 set -gx fish_color_comment 908caa
 # set -gx fish_color_match --background=brblue
-set -gx fish_color_selection white --bold --background=brblack
-# set -gx fish_color_search_match bryellow --background=brblack
+set -gx fish_color_selection --reverse
 # set -gx fish_color_history_current --bold
-set -gx fish_color_operator 9ccfd8
-set -gx fish_color_escape 9ccfd8
+set -gx fish_color_operator e0def4
+set -gx fish_color_escape 31748f
 set -gx fish_color_autosuggestion 908caa
-set -gx fish_color_cwd green
+set -gx fish_color_cwd ebbcba
 # set -gx fish_color_cwd_root red
-# set -gx fish_color_valid_path --underline
-set -gx fish_color_user brgreen
-set -gx fish_color_host normal
-# set -gx fish_color_host_remote magenta
-set -gx fish_color_cancel -r
+set -gx fish_color_user f6c177
+set -gx fish_color_host 9ccfd8
+set -gx fish_color_host_remote c4a7e7
+set -gx fish_color_cancel e0def4
+set -gx fish_color_search_match --background=191724
+set -gx fish_color_valid_path
 
 # pager color variables
 # https://fishshell.com/docs/current/interactive.html#pager-color-variables
-set -gx fish_pager_color_progress cyan
-set -gx fish_pager_color_background
-set -gx fish_pager_color_prefix blue
-set -gx fish_pager_color_completion normal
-set -gx fish_pager_color_description normal
-set -gx fish_pager_color_selected_background --reverse
-set -gx fish_pager_color_selected_prefix
-set -gx fish_pager_color_selected_completion
-set -gx fish_pager_color_selected_description
+set -gx fish_pager_color_progress ebbcba
+set -gx fish_pager_color_background --background=1f1d2e
+set -gx fish_pager_color_prefix 9ccfd8
+set -gx fish_pager_color_completion 908caa
+set -gx fish_pager_color_description 908caa
 set -gx fish_pager_color_secondary_background
-set -gx fish_pager_color_secondary_prefix blue
-set -gx fish_pager_color_secondary_completion normal
-set -gx fish_pager_color_secondary_description normal
-# set -gx fish_pager_color_completion normal
-# set -gx fish_pager_color_description ebbcba yellow
-# set -gx fish_pager_color_prefix white --bold --underline
-# set -gx fish_pager_color_progress brwhite --background=cyan
+set -gx fish_pager_color_secondary_prefix
+set -gx fish_pager_color_secondary_completion
+set -gx fish_pager_color_secondary_description
+set -gx fish_pager_color_selected_background --background=26233a
+set -gx fish_pager_color_selected_prefix 9ccfd8
+set -gx fish_pager_color_selected_completion e0def4
+set -gx fish_pager_color_selected_description e0def4
